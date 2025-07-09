eb5a01730edb0436d7527a37c91b2af0838e55d3
wjsc <walterscarano@outlook.com>
2025-07-09 19:08:08 -0300

    client connected to ollama

--- mcp-client/index.js
+++ mcp-client/index.js
@@ -1,8 +1,11 @@
 const { Client } = require("@modelcontextprotocol/sdk/client/index.js")
 const { StreamableHTTPClientTransport } = require( "@modelcontextprotocol/sdk/client/streamableHttp.js")
+const { Ollama } = require('ollama') 
+
 
 let client = undefined
-const baseUrl = new URL("http://localhost:4000/mcp");
+const baseUrl = new URL("http://127.0.0.1:4000/mcp");
+const ollama = new Ollama({host: 'http://127.0.0.1:11434'})
 
 const run = async() => {
     try {
@@ -14,17 +17,32 @@ const run = async() => {
             new URL(baseUrl)
         );
 
-       
-
         await client.connect(transport);
         console.log("Connected using Streamable HTTP transport");
         const tools = await client.listTools();
 
-        console.log(tools)
+
+        const response = await ollama.chat({
+            model: 'qwen2.5-coder:7b-instruct',
+            messages: [
+                { 
+                    role: 'system',
+                    content: 'You are connected to a custom mpc server with the following available tools: ' + JSON.stringify(tools)
+                },
+                {
+                    role: 'system',
+                    content: 'Please provide an explanation of the available tools and their purpose.'
+                }
+            
+            ],
+        })
+
+
+        console.log(response)
+
         
     } catch (error) {
 
-        console.log("Streamable HTTP connection failed");
         console.log(error)
         
     }
